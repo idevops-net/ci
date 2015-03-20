ead42b5c490e5c39635a72a107d5b3db619b9938
gengjh <gengjh@cn.ibm.com>
2015-03-20 23:34:54 +0800

    Add gradle_helper

--- scripts/init_jenkins_env.sh
+++ scripts/init_jenkins_env.sh
@@ -2,37 +2,18 @@
 
 # By default the path of the tool
 BUILDTOOL_PATH=${BUILDTOOL_PATH:-`pwd`/buildtool}
-GRADLE_VERSION=2.3
+
+source ./devops-infra/lib/gradle_helper
+
 pushd `pwd`
 mkdir -p $BUILDTOOL_PATH
 
 #######################
 ## Install necessary RPM
 echo "Install RPM packages!!"
-yum install -y java git python
+yum install -y java git python python-virtualenv
 
-#######################
-## Prepare gardle env
-echo "Prepare gradle environment!!"
-wget --no-check-certificate -O gradle-$(GRADLE_VERSION)-bin.zip https://services.gradle.org/distributions/gradle-$(GRADLE_VERSION)-bin.zip
-cd $BUILDTOOL_PATH; tar -zxf gradle-$(GRADLE_VERSION)-bin.zip
-GRADLE_PATH=$BUILDTOOL_PATH/gradle-$(GRADLE_VERSION)
-cat >> $GRADLE_PATH/build.gradle << EOF
-task wrapper(type: Wrapper) {
-    gradleVersion = '$(GRADLE_VERSION)'
-}
-EOF
-# Build gradle wrapper
-cd $GRADLE_PATH
-mkdir -p gradle/wrapper
-./bin/gradle wrapper
-cat >> gradle/wrapper/gradle-wrapper.properties << EOF
-distributionBase=GRADLE_USER_HOME
-distributionPath=wrapper/dists
-zipStoreBase=GRADLE_USER_HOME
-zipStorePath=wrapper/dists
-distributionUrl=$BUILDTOOL_PATH/gradle-$GRADLE_VERSION-bin.zip
-EOF
+prepare_gradlew
 
 #######################
 ## Prepare fpm
